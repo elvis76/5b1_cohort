58640064b574cc2605c533f06a118b216dece5d8
elvis <elvisduru76@gmail.com>
2022-01-18 15:51:56 +0100

    variables, values, expressions, operators

---
+++ 5B1_cohort/first_class.py
@@ -0,0 +1,47 @@
+# print("Welcome to python")
+
+#checking types
+# print(type("5"))
+# print(type(5))
+# print(type(5.0))
+
+#changing types
+
+# from turtle import st
+
+
+# print(type(int("5")))
+
+# #assigning variables
+
+# a = "This is a string"
+# print(a)
+
+# a =5
+# b =10
+# c =3
+
+# answer = (-b+(4*a*c)**0.5)/(2*a)
+
+# print(answer)
+
+# easypace = ((8*60) + 15)*2
+# tempopace = ((7*60) + 12)*3
+
+# my_runtime = easypace + tempopace
+# print (my_runtime)
+
+# my_answer = my_runtime / 3600
+# start = 6 + (52/60)
+# print(my_answer + start)
+
+# volume = (4/3)*3.14*(5**3)
+# print(volume)
+
+# price_for_60 = (24.95*60)*0.6
+
+# shipping = 3 + (0.75*59)
+
+# print(price_for_60+shipping)
+
+
